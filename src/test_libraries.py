f65fa84d502b7bad71a53af8ce58594af1691950
s-vanb <scott.vanbolhuis@gmail.com>
2024-03-28 15:58:19 -0700

    Initial project setup with virtual environment and dependencies

---
+++ src/test_libraries.py
@@ -0,0 +1,8 @@
+import numpy
+import scipy
+import matplotlib.pyplot as plt
+import librosa
+import tensorflow as tf
+
+print("All libraries installed successfully.")
+print(f"TensorFlow version: {tf.__version__}")
